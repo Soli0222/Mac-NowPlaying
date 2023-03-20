9a33236fd1d594f030b15dc51d99fdd96d6e7152
Soli <Soli0222@proton.me>
2023-03-21 01:20:52 +0900

    Swinsianで再生してる曲を取得できた

---
+++ main.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def get_current_track_info():
+    script = '''
+        tell application "Swinsian"
+            if it is running then
+                if player state is playing then
+                    set track_name to name of current track
+                    set artist_name to artist of current track
+                    set album_name to album of current track
+                    return {track_name, artist_name, album_name}
+                end if
+            end if
+        end tell
+    '''
+    p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate(script.encode('utf-8'))
+    if stderr:
+        raise Exception(stderr.decode('utf-8').strip())
+    if "missing value" in stdout.decode('utf-8').strip():
+        return None
+    track_name, artist_name, album_name = stdout.decode('utf-8').strip()[1:-1].split(', ')
+    return track_name, artist_name, album_name
+
+if __name__ == "__main__":
+    track_name, artist_name, album_name = get_current_track_info()
